5783bae06ad92f8ae7cdb3198ec3a67409ad344d
bennettricki <bennett.ricki@proton.me>
2024-07-12 16:58:01 +0000

    Update proxy_scraper.py

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -62,15 +62,12 @@ def check_risk_level(proxy, proxy_type):
 def main():
     urls = {
         'http': ['https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
-                'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/http.txt',
-                'https://github.com/zloi-user/hideip.me/raw/main/http.txt',
-                'https://github.com/zloi-user/hideip.me/raw/main/https.txt'],
+                'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/http.txt'
+                ],
         'socks4': ['https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt',
-                  'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks4.txt',
-                  'https://github.com/zloi-user/hideip.me/raw/main/socks4.txt'],
+                  'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks4.txt'],
         'socks5': ['https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt',
-                  'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks5.txt',
-                  'https://github.com/zloi-user/hideip.me/raw/main/socks5.txt']
+                  'https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks5.txt']
     }
 
     all_proxies = defaultdict(set)
